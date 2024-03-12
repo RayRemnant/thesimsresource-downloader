cd64e503a2dc68e1b5dc2772befceb7a453098fa
RayRemnant <49475467+RayRemnant@users.noreply.github.com>
2024-03-12 12:12:42 +0100

    changed console logs slightly

--- app/api/db/_cookiesStorage.js
+++ app/api/db/_cookiesStorage.js
@@ -1,24 +1,21 @@
-const fs = require('fs').promises;
+const fs = require("fs").promises;
 
 exports.get = async function get() {
-	try {
-		const cookies = await fs.readFile("generated/cookies.json", 'utf8')
-		console.log("COOKIES RETRIEVED: ")
-		return JSON.parse(cookies)
-	} catch (e) {
-		return []
-	}
-
-}
+  try {
+    const cookies = await fs.readFile("generated/cookies.json", "utf8");
+    console.log("COOKIES RETRIEVED");
+    return JSON.parse(cookies);
+  } catch (e) {
+    return [];
+  }
+};
 
 exports.save = async function save(newCookies) {
-
-	await fs.writeFile("generated/cookies.json", JSON.stringify(newCookies))
-	console.log("COOKIES SAVED: ")
-}
+  await fs.writeFile("generated/cookies.json", JSON.stringify(newCookies));
+  console.log("COOKIES SAVED");
+};
 
 exports.delete = async function del() {
-
-	await fs.writeFile("generated/cookies.json", "")
-	console.log("COOKIES DELETED")
-}
+  await fs.writeFile("generated/cookies.json", "");
+  console.log("COOKIES DELETED");
+};
